0eae72b4124a04a59dc7e33016a59f534799ed9d
Kenneth Onah <kenneth@netcraft-devops.com>
2017-01-26 13:34:52 +0200

    refactor test suites to make is simpler

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,14 @@
 <?php
 
-require_once __DIR__ . '/TestCase.php';
-require realpath(dirname(__FILE__)) . '/../vendor/autoload.php';
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+// To suppress the warning during the date() invocation in logs, we would default the timezone to GMT.
+if (!ini_get('date.timezone')) {
+    date_default_timezone_set('GMT');
+}
+// Include the composer autoloader
+$loader = require dirname(__DIR__) . '/vendor/autoload.php';
+$loader->add('PayU\\Test', __DIR__);
+if (!defined("PP_CONFIG_PATH")) {
+    define("PP_CONFIG_PATH", __DIR__);
+}

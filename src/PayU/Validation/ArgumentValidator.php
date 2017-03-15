f1de0d0d8a24d732eed8a5a7712bb59ac21c4774
Kenneth Onah <kenneth@netcraft-devops.com>
2017-03-16 01:48:01 +0200

    validators

--- src/PayU/Validation/ArgumentValidator.php
+++ src/PayU/Validation/ArgumentValidator.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * PayU MEA PHP SDK
+ *
+ * @copyright  Copyright (c) 2016 PayU
+ * @license    http://opensource.org/licenses/LGPL-3.0  Open Software License (LGPL 3.0)
+ * @link http://www.payu.co.za
+ * @link http://help.payu.co.za/developers
+ * @author Kenneth Onah <kenneth@netcraft-devops.com>
+ */
 
 namespace PayU\Validation;
 
@@ -6,15 +15,9 @@ namespace PayU\Validation;
  * Class ArgumentValidator
  *
  * @package PayU\Validation
- * @copyright  Copyright (c) 2016 PayU
- * @license    http://opensource.org/licenses/LGPL-3.0  Open Software License (LGPL 3.0)
- * @link http://www.payu.co.za
- * @link http://help.payu.co.za/developers
- * @author Kenneth Onah <kenneth@netcraft-devops.com>
  */
 class ArgumentValidator
 {
-
     /**
      * Helper method for validating an argument that will be used by this API in any requests.
      *
--- src/PayU/Validation/UrlValidator.php
+++ src/PayU/Validation/UrlValidator.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * PayU MEA PHP SDK
+ *
+ * @copyright  Copyright (c) 2016 PayU
+ * @license    http://opensource.org/licenses/LGPL-3.0  Open Software License (LGPL 3.0)
+ * @link http://www.payu.co.za
+ * @link http://help.payu.co.za/developers
+ * @author Kenneth Onah <kenneth@netcraft-devops.com>
+ */
 
 namespace PayU\Validation;
 
@@ -6,11 +15,6 @@ namespace PayU\Validation;
  * Class UrlValidator
  *
  * @package PayU\Validation
- * @copyright  Copyright (c) 2016 PayU
- * @license    http://opensource.org/licenses/LGPL-3.0  Open Software License (LGPL 3.0)
- * @link http://www.payu.co.za
- * @link http://help.payu.co.za/developers
- * @author Kenneth Onah <kenneth@netcraft-devops.com>
  */
 class UrlValidator
 {
